967ac2c876fde0de86f205fabc20f9a156352119
lovanto <rifkylovanto@gmail.com>
2020-04-11 21:31:20 +0700

    config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ date_default_timezone_set("Asia/Jakarta");
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = "http://".$_SERVER['HTTP_HOST'];
+$config['base_url'] = "https://pos-pam.herokuapp.com/welcome";
 $config['base_url'] .= preg_replace('@/+$@','',dirname($_SERVER['SCRIPT_NAME'])).'/';
 
 /*
